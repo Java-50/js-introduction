28fd8dd437a14e46ca3c81c2fd08a2eb80defddd
Vlad Zlatopolsky <vladislav.zlatopolsky@gmail.com>
2023-01-21 23:27:50 +0200

    adds hw_11 solution

--- HW#11_solution.js
+++ HW#11_solution.js
@@ -1,49 +1,3 @@
-//numbers
-// let num = 25.325;
-// num = 40.5;
-
-//num = Math.floor(num);
-//num = Math.trunc(num);
-//num = Math.ceil(num);
-// num = Math.round(num);
-// num **= 2; // num = num ** 2
-
-//strings
-// let str = "a'bc'd";
-// let str1 = 'a"bc"d';
-// let res = +"123" + +56;
-// let res1 = "123" - 56;
-// let res2 = Math.round("123" / 56);
-// let res3 = "123" / "2";
-// let res4 = "ab" / 8;
-
-//boolean
-// let a = 100;
-// let error = true;
-// let res = true + true;
-// let res1 = true && false;
-// let res2 = a || 256 / a;
-// let message = error && "some error happenning";
-// let re3 = a || 10;
-// let res4 = "123" > "9";
-// let res5 = "123" > 9;
-//equilty operators
-// let res10 = "123" == 123; //true
-// let res11 = "123" === 123; //false
-// console.log(res11);
-
-
-
-
-
-
-
-
-
-
-//string functions for HW
-// let str = "aBdTYg";
-// str = str.toLowerCase(); //"abcdtyg"
 
 // HW #11
 function getDigitsSum(number) {
@@ -120,36 +74,4 @@ console.log("Expected = 4321; Actual = "+ reverse(1234));
 console.log("Expected = 321; Actual = "+ reverse(123.45));
 console.log("Expected = -082; Actual = "+ reverse(-280.123));
 console.log("Expected = 0; Actual = "+ reverse(0));
-
-
-
-// let n = 10;
-// let count = 0;
-// while (n > 0) {
-//     n--;
-//     count++;
-// }
-// do {
-//   n--;
-//   count++
-// } while(n > 0);
-// while(--n) {
-//     count++;
-// }
-// let d;
-// 3 + 7 * "10"
-
-
-
-
- 
-
-
-
-
-
-
-
-
-
 
